86d90ce5581d147ca012120e6a6e06cfe2ba767f
valentegarcia-web <valente.garcia@confidelis.mx>
2026-03-30 15:44:06 -0600

    Remove Python version info from script

--- Generar estado de cuenta.py
+++ Generar estado de cuenta.py
@@ -1,5 +1,3 @@
-Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
